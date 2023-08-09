55467728a3d033a14fcc58e5a723588fe5b8bf78
KoudaiKatayama <132341723+koudaikatayama@users.noreply.github.com>
2023-08-09 06:22:30 +0000

    controller_routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :users,skip: [:passwords], controllers: {
@@ -14,10 +15,27 @@ Rails.application.routes.draw do
   
   scope module: :public do
     root to: "homes#top"
+    
+    resources :posts, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+      resources :comments, only: [:create, :destroy]
+      resource :likes, only: [:create, :destroy]
+    end
+    
+    get "users/mypage" => "users#show"
+    get "users/information/edit" => "users#edit"
+    patch "users/information" => "users#update"
+    get "/users/check" => "users#check"
+    patch "/users/withdraw" => "usres#withdraw"
   end
   
   namespace :admin do
     get "/" => "homes#top"
+    
+    resources :genres, only: [:index, :create, :edit, :update]
+    
+    resources :users, only: [:index, :show, :edit, :update, :destroy]
+    
+    resources :posts, only: [:index, :show, :edit, :update, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
