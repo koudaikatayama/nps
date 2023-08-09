fb2e3763013848055890e0fb49755dc35a384d20
KoudaiKatayama <132341723+koudaikatayama@users.noreply.github.com>
2023-08-09 04:49:34 +0000

    bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,13 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+  
+  scope module: :public do
+    root to: "homes#top"
+  end
+  
+  namespace :admin do
+    get "/" => "homes#top"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
