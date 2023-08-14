c098a5502cb575463ee0c0fd91963b731bebd07a
KoudaiKatayama <132341723+koudaikatayama@users.noreply.github.com>
2023-08-14 15:09:19 +0000

    admin_post_index

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,16 +1,31 @@
 class Admin::PostsController < ApplicationController
+  before_action :authenticate_admin!
+  
   def index
+    @posts = Post.all.page(params[:page]).per(10)
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
   
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to admin_post_psth(@post)
   end
   
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to admin_posts_psth
+  end
+  
+  def post_params
+    params.require(:post).permit(:name, :explanation, :address, :spot_image, :genre_id, tag_ids: [])
   end
 end
